54c69ecbd851f03e3df8e98d9a6c081d6038010b
muiz332 <muizzuddin332@gmail.com>
2022-10-12 20:02:49 +0700

    perbaikan materi lingkup pengembang javascript

--- 1.dasar pemrogaman dengan javascript/07.lingkungan pengembangan javascript/script.js
+++ 1.dasar pemrogaman dengan javascript/07.lingkungan pengembangan javascript/script.js
@@ -1,46 +1,50 @@
-/*lingkungan pengembang javascript 
+/* lingkungan pengembang javascript 
 
-jadi lingkungan pengembangan atau development environment adalah sebuah sistem atau tools dimana nantinya kita akan menuliskan progam kita biasanya untuk bahasa bahasa pemrogaman lain seperti java c atau python itu ada yang kita instal dan konfigutasi terlebih dahulu misalnya kita instal compiler virtual mechine atau progam pendukung lainnya 
+jadi lingkungan pengembangan atau development environment adalah sebuah sistem atau tools dimana nantinya kita akan menuliskan progam kita biasanya
+untuk bahasa bahasa pemrogaman lain seperti java c atau python itu ada yang kita instal dan konfigutasi terlebih dahulu misalnya kita instal 
+compiler virtual mechine atau progam pendukung lainnya 
 
 
 nah untungnya dengan javascript kita tidak perlu melakukan itu semua karena pada umumnya kalian sudah punya lingkungan pengembangan javascript 
-
 kenapa? karena pasti kaian sudah punya web browser dikomputer atau laptop kalian jadi tiap tiap browser itu ada interpreter javascript didalamnya 
 
 nah nanti kalian boleh pakai web browser apapun untuk mengikuti seri ini namun yang saya gunakan disini nantinya adalah google crome 
-
 nah selanjutnya ada dua cara agar kita dapat berinterasi dengan interpreter javascript yang ada dibrowser kita 
 
 1.console
-nah yang pertama itu adalah dengan menggunakan console yang ada dideveloper tools dari tiap tiap browser 
- nah nanti saya akan kasih contoh bagaimana cara mengakses console pada google crome 
- tapi sebelumnya kita harus punya dulu halaman web kosong agar kita bisa mengakses consolenya 
- jadi kita buat dulu halaman html sederhana 
- 
- untuk mengakses console dengan cara klik kanan inspect lalu klik console nanti akan muncul console javascriptnya 
- atau kalian bisa memunculkan ini dengan cara lain klik titik tiga yang ada dibrowser lali klik more tools lalu klik developer tools
+    yang pertama itu adalah dengan menggunakan console yang ada dideveloper tools dari tiap tiap browser, nanti saya akan kasih contoh bagaimana 
+    cara mengakses console pada google crome, tapi sebelumnya kita harus punya dulu halaman web kosong agar kita bisa mengakses consolenya 
+    jadi kita buat dulu halaman html sederhana 
 
- apa saja sih yang kita bisa lalukan dengan console, jadi console ini penting banget untuk kalian kenali yang pertama itu kalian bisa menuliskan perintah javascript atau sintax javascript langsung diconsole ini misalnya tulis saja alert ("hello world"); nanti akan muncul pop up atau muncul tulisan dilayar dengan nama hello world kalian tinggal pencet enter saja untuk menjalankannya 
+    untuk mengakses console dengan cara klik kanan inspect lalu klik console nanti akan muncul console javascriptnya 
+    atau kalian bisa memunculkan ini dengan cara lain klik titik tiga yang ada dibrowser lali klik more tools lalu klik developer tools
 
- bisa menggunakan tanda kutip "" atau tanda kutip yang '' sama saja 
- 
- nah yang ke dua kalian bisa menggunakan console ini untuk mengetahui kalo misalkan kalian ada eror atau kesalahan pada progam javascript kalian nanti console ini akan memberitahu erornya apa terjadi difile mana terjadi dibaris berapa
- 
- jadi itu sangat membudahkan 
- 
- atau yang ketiga kalian bisa menggunakan console ini untuk melakuan debug pada progam kalian 
- nanti kita akan sering banget menggunakan fungsi yang namanya console.log yang kalian tuliskan discript javscriptnya untuk memunculkan sesuatu didalam console ini 
- 
- mungkin nanti contohnya kita akan coba ya 
+    apa saja sih yang kita bisa lalukan dengan console, jadi console ini penting banget untuk kalian kenali, yang pertama itu kalian bisa menuliskan
+    perintah javascript atau sintax javascript langsung diconsole, misalnya tulis saja alert ("hello world"); nanti akan muncul pop up atau muncul
+    tulisan dilayar dengan nama hello world kalian tinggal pencet enter saja untuk menjalankannya 
+
+    bisa menggunakan tanda kutip "" atau tanda kutip yang '' sama saja 
+
+    nah yang ke dua kalian bisa menggunakan console ini untuk mengetahui kalo misalkan kalian ada eror atau kesalahan pada progam javascript 
+    kalian nanti console ini akan memberitahu erornya apa terjadi difile mana terjadi dibaris berapa
+
+    jadi itu sangat memudahkan atau yang ketiga kalian bisa menggunakan console ini untuk melakuan debug pada progam kalian 
+    nanti kita akan sering banget menggunakan fungsi yang namanya console.log yang kalian tuliskan discript javscriptnya untuk memunculkan 
+    sesuatu didalam console ini 
+
+    mungkin nanti contohnya kita akan coba ya 
  
  
 2.menuliskan tag script dihalaman html 
- nah yang kedua bagaimana kita berinteraksi dengan javascript adalah dengan menuliskan script javascriptnya sendiri didalam htmlnya 
+    nah yang kedua bagaimana kita berinteraksi dengan javascript adalah dengan menuliskan script javascriptnya sendiri didalam htmlnya 
+
+    nah cara menuliskannya nanti ada dua cara kita bisa simpan difile yang sama di dalam htmlnya atau kita bisa membuat file javascript terpisah 
+    ini mirip seperti css ya 
+
+    nah kita akan coba ya 
+    kita pergi kedalam htmlnya dulu 
  
- nah cara menuliskannya nanti ada dua cara kita bisa simpang difile yang sama di dalam htmlnya atau kita bisa membuat file javascript terpisah ini mirip seperti css ya 
- 
- nah kita akan coba ya 
- kita pergi kedalam htmlnya dulu */
+*/
 
  alert ("hello world"); 
 
